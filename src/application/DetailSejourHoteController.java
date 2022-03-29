588cd347eefefca623314b42723158203ebc01ac
ElodieGiryTD3 <elodie.giry@alumni.univ-avignon.fr>
2022-03-29 15:54:29 +0200

    Ajout du lien pour bouton détail séjour vers liste séjour Ajout fenêtre concept Adéona

--- src/application/DetailSejourHoteController.java
+++ src/application/DetailSejourHoteController.java
@@ -1,5 +1,26 @@
 package application;
 
-public class DetailSejourHoteController {
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
+public class DetailSejourHoteController {
+	
+	@FXML
+	private Button btnValiderSejourHote;
+
+	public void validerSejourHote() {
+		Scene scene = btnValiderSejourHote.getScene();
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("ListeSejourHoteBarreRecherche.fxml"));
+			scene.setRoot(root);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
+
