956b5de50332b20aa196da52d369e097aad4e9cc
ElodieGiryTD3 <elodie.giry@alumni.univ-avignon.fr>
2022-04-13 16:12:55 +0200

    Commit pour merger

--- src/application/DetailSejourHoteController.java
+++ src/application/DetailSejourHoteController.java
@@ -5,19 +5,52 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class DetailSejourHoteController {
 	
 	@FXML
 	private Button btnValiderSejourHote;
-	
 	@FXML
 	private Button btnConcept;
+	
+	/*
+	@FXML 
+	private TextField localisation, periodeDeb, periodeFin, logementAutre, serviceAutre, dureeQuotidienne, contrainte, nbVoyageur, contrepartieAutre;
 
+	@FXML 
+	private RadioButton cabane, chambre, jardinage, bricolage, cuisine, petitDej, demiPension, pensionComplete, baladeCheval, blanchissage, piscine, transfertOui, transfertNon;
+	
+	private String localisation1, periodeDeb1, periodeFin1, logementAutre1, serviceAutre1, dureeQuotidienne1, contrainte1, nbVoyageur1, contrepartieAutre1;
+	
+	*/
 	public void validerSejourHote() {
 		Scene scene = btnValiderSejourHote.getScene();
 		try {
+			
+			/*
+			localisation1 = localisation.getText();
+			//récupérer valeur calendrier ?
+			logementAutre1 = logementAutre.getText();
+			serviceAutre1 = serviceAutre.getText();
+			dureeQuotidienne1 = dureeQuotidienne.getText();
+			contrainte1 = contrainte.getText();
+			nbVoyageur1 = nbVoyageur.getText();
+			contrepartieAutre1 = contrepartieAutre.getText();
+			
+			
+			System.out.println(localisation1);
+			System.out.println(logementAutre1);
+			System.out.println(serviceAutre1);
+			System.out.println(dureeQuotidienne1);
+			System.out.println(contrainte1);
+			System.out.println(nbVoyageur1);
+			System.out.println(contrepartieAutre1);
+			*/
+			
+			
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("ListeSejourHoteBarreRecherche.fxml"));
 			scene.setRoot(root);
 		} 
