907d8756ced6708aee219511172b1e495af52ee2
BERTHORELLY Lea <73932490+Lea-BERTHORELLY@users.noreply.github.com>
2022-04-23 22:44:03 +0200

    Update SearchController.java

--- src/application/controllers/SearchController.java
+++ src/application/controllers/SearchController.java
@@ -3,15 +3,21 @@ package application.controllers;
 import application.Main;
 import application.models.Voyage;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,15 +26,23 @@ import java.util.ArrayList;
 
 public class SearchController {
 
-    @FXML private TextField txtRecherche2;
-    @FXML private DatePicker debut_sejour2;
-    @FXML private DatePicker fin_sejour2;
-    @FXML private Text txtWarning2;
+    @FXML
+    private TextField txtRecherche2;
+    @FXML
+    private DatePicker debut_sejour2;
+    @FXML
+    private DatePicker fin_sejour2;
+    @FXML
+    private Text txtWarning2;
 
-    @FXML private VBox tripListView = null;
-    @FXML private ImageView imgMap;
-    @FXML private ScrollPane scrollPaneResults;
-    @FXML private Text txtResults;
+    @FXML
+    private VBox tripListView = null;
+    @FXML
+    private ImageView imgMap;
+    @FXML
+    private ScrollPane scrollPaneResults;
+    @FXML
+    private Text txtResults;
     private boolean isAddingCards = false;
     private int startIndexToAddCards;
 
@@ -51,7 +65,7 @@ public class SearchController {
 
         debut_sejour2.valueProperty().addListener((ov, oldValue, newValue) -> {
             updateDatePicker(fin_sejour2, debut_sejour2.getValue());
-            if (fin_sejour2.getValue().isBefore(debut_sejour2.getValue())){
+            if (fin_sejour2.getValue().isBefore(debut_sejour2.getValue())) {
                 fin_sejour2.setValue(debut_sejour2.getValue());
             }
             searchVoyages();
@@ -70,7 +84,7 @@ public class SearchController {
         searchVoyages();
     }
 
-    public void searchVoyages(){
+    public void searchVoyages() {
         if (txtRecherche2.getText().length() > 1) {
             listVoyagesResults2 = searchVoyagesResults2();
             setListVoyagesResults(listVoyagesResults2);
@@ -80,13 +94,13 @@ public class SearchController {
         }
     }
 
-    public void displaySearchResults(){
+    public void displaySearchResults() {
         scrollPaneResults.setVvalue(0);
         displayResultsQty();
         initializeTripListView();
     }
 
-    public void displayResultsQty(){
+    public void displayResultsQty() {
         // gère le nombre de résultats
         if (listVoyagesResults2.size() == 0) txtResults.setText("Désolé, nous n'avons trouvé aucun résultat...");
         else txtResults.setText(listVoyagesResults2.size() + " séjours peuvent vous satisfaire");
@@ -94,22 +108,22 @@ public class SearchController {
         scrollPaneResults.setVisible(listVoyagesResults2.size() != 0);
     }
 
-    public void initializeTripListView(){
+    public void initializeTripListView() {
         startIndexToAddCards = 0;
         tripListView.getChildren().clear();
-        if (listVoyagesResults2.size() > 0){
+        if (listVoyagesResults2.size() > 0) {
             int numberOfCardsToAdd = 10;
             if (listVoyagesResults2.size() < 10) numberOfCardsToAdd = listVoyagesResults2.size();
             addCardToTripListView(numberOfCardsToAdd, startIndexToAddCards);
         }
     }
 
-    public void addCardToTripListView(int numberOfCards, int startIndex){
+    public void addCardToTripListView(int numberOfCards, int startIndex) {
         Node[] cardsToAdd = new Node[numberOfCards];
         // initialize graphics
-        for (int i=0; i<cardsToAdd.length;i++){
-            try{
-                final int j=i;
+        for (int i = 0; i < cardsToAdd.length; i++) {
+            try {
+                final int j = i;
                 cardsToAdd[i] = FXMLLoader.load(Main.class.getResource("views/trip-card-view.fxml"));
                 cardsToAdd[i].setStyle("-fx-border-color: lightgrey");
 
@@ -121,14 +135,14 @@ public class SearchController {
                 Label heures = (Label) cardsToAdd[i].lookup("#heures");
                 Label indexForTest = (Label) cardsToAdd[i].lookup("#indexForTest");
 
-                File file = new File("src/application/assets/images/"+ listVoyagesResults2.get(startIndex+i).getVille() +".png");
+                File file = new File("src/application/assets/images/ville/" + listVoyagesResults2.get(startIndex + i).getVille() + ".png");
                 Image image = new Image(file.toURI().toString());
                 iv.setImage(image);
-                titre.setText(listVoyagesResults2.get(startIndex+i).getContreparties() + " à " + listVoyagesResults2.get(startIndex+i).getVille());
-                contrepartie.setText(listVoyagesResults2.get(startIndex+i).getContreparties());
-                logement.setText(listVoyagesResults2.get(startIndex+i).getType());
-                heures.setText(listVoyagesResults2.get(startIndex+i).getHeure() + " h/j");
-                indexForTest.setText("("+(startIndex+i+1)+")");
+                titre.setText(listVoyagesResults2.get(startIndex + i).getContreparties() + " à " + listVoyagesResults2.get(startIndex + i).getVille());
+                contrepartie.setText(listVoyagesResults2.get(startIndex + i).getContreparties());
+                logement.setText(listVoyagesResults2.get(startIndex + i).getType());
+                heures.setText(listVoyagesResults2.get(startIndex + i).getHeure() + " h/j");
+                indexForTest.setText("(" + (startIndex + i + 1) + ")");
 
                 // add some effect
                 cardsToAdd[i].setOnMouseEntered(event -> {
@@ -138,19 +152,29 @@ public class SearchController {
                     cardsToAdd[j].setStyle("-fx-border-color: lightgrey");
                 });
 
+                // add onClickListener
+                int voyageIndex = startIndex + i;
+                cardsToAdd[i].setOnMouseClicked((new EventHandler<MouseEvent>() {
+                    public void handle(MouseEvent event) {
+                        System.out.println("VoyageIndex: " + voyageIndex);
+                        navigateToDetailVoyageView(listVoyagesResults2.get(voyageIndex), event);
+                    }
+                }));
+
                 // add items
                 tripListView.getChildren().add(cardsToAdd[i]);
-            } catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         startIndexToAddCards = startIndex + numberOfCards;
     }
 
-    @FXML void onScrollFinished(ScrollEvent event){
-        if (!isAddingCards){
+    @FXML
+    void onScrollFinished(ScrollEvent event) {
+        if (!isAddingCards) {
             isAddingCards = true;
-            if (scrollPaneResults.getVvalue() == 1 && tripListView.getChildren().size() < listVoyagesResults2.size()){
+            if (scrollPaneResults.getVvalue() == 1 && tripListView.getChildren().size() < listVoyagesResults2.size()) {
                 System.out.println("adding new cards");
                 //define number of cards to add
                 int numberOfCardsToAdd = 10;
@@ -163,7 +187,7 @@ public class SearchController {
         }
     }
 
-    public void setMap(){
+    public void setMap() {
         File file = new File("src/application/assets/map.png");
         Image image = new Image(file.toURI().toString());
         imgMap.setImage(image);
@@ -189,22 +213,22 @@ public class SearchController {
         this.fin_sejour2.setValue(fin_sejour2.getValue());
     }
 
-    public ArrayList<Voyage> searchVoyagesResults2(){
+    public ArrayList<Voyage> searchVoyagesResults2() {
         System.out.println(this.txtRecherche2.getText());
-        if (debut_sejour2.getValue() != null)	System.out.println(this.debut_sejour2.getValue());
-        if (fin_sejour2.getValue() != null)	System.out.println(this.fin_sejour2.getValue());
+        if (debut_sejour2.getValue() != null) System.out.println(this.debut_sejour2.getValue());
+        if (fin_sejour2.getValue() != null) System.out.println(this.fin_sejour2.getValue());
 
         // FOR DEMO: if input= "all", results if all 10000 voyages
-        if (txtRecherche2.getText().equals("all")){
+        if (txtRecherche2.getText().equals("all")) {
             return this.listAllVoyages;
         }
 
         ArrayList<Voyage> results = new ArrayList<Voyage>();
 
-        for (Voyage v: listAllVoyages){
+        for (Voyage v : listAllVoyages) {
             if ((v.getVille().contains(txtRecherche2.getText()) || v.getContreparties().contains(txtRecherche2.getText()) || v.getType().contains(txtRecherche2.getText()))
-                    && (debut_sejour2.getValue() == null || !debut_sejour2.getValue().isBefore(LocalDate.parse(v.getDateArrivee())) )
-                    && (fin_sejour2.getValue() == null || !fin_sejour2.getValue().isAfter(LocalDate.parse(v.getDateDepart())) ) ){
+                    && (debut_sejour2.getValue() == null || !debut_sejour2.getValue().isBefore(LocalDate.parse(v.getDateArrivee())))
+                    && (fin_sejour2.getValue() == null || !fin_sejour2.getValue().isAfter(LocalDate.parse(v.getDateDepart())))) {
                 results.add(v);
             }
         }
@@ -212,24 +236,46 @@ public class SearchController {
         return results;
     }
 
-    public void displayWarning(){
+    public void displayWarning() {
         txtWarning2.setVisible(true);
-        new Thread( new Runnable() {
-            public void run()  {
-                try  { Thread.sleep( 3000 ); }
-                catch (InterruptedException ie)  {}
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException ie) {
+                }
                 txtWarning2.setVisible(false);
             }
-        } ).start();
+        }).start();
     }
 
-    public void updateDatePicker(DatePicker dp, LocalDate startLocalDate){
+    public void updateDatePicker(DatePicker dp, LocalDate startLocalDate) {
         // restriction sur date picker
         dp.setDayCellFactory(picker -> new DateCell() {
             public void updateItem(LocalDate date, boolean empty) {
                 super.updateItem(date, empty);
-                setDisable(empty || date.compareTo(startLocalDate) < 0 );
+                setDisable(empty || date.compareTo(startLocalDate) < 0);
             }
         });
+    }
+
+    public void navigateToDetailVoyageView(Voyage v, MouseEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/voyage-detail-view.fxml"));
+        Parent root = null;
+        try {
+            root = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        DetailVoyageController detailVoyageController = (DetailVoyageController) fxmlLoader.getController();
+        detailVoyageController.setVoyage(v);
+        detailVoyageController.init();
+
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
     }
 }
