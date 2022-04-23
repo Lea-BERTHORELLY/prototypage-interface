a6d60b284c804be074d40a2d57d90178b2405193
BERTHORELLY Lea <leabty.lb@gmail.com>
2022-04-23 22:32:04 +0200

    Correction de bugs

--- src/application/AccueilController.java
+++ src/application/AccueilController.java
@@ -1,20 +1,7 @@
 package application;
 
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -49,7 +36,6 @@ import javafx.stage.Stage;
 
 public class AccueilController implements Initializable{
 	@FXML private Pane contact;
-	@FXML private ImageView img1,img2,img3,img4,img5;
 	@FXML private TextField txtRecherche;
 	@FXML private DatePicker debut_sejour;
 	@FXML private DatePicker fin_sejour;
@@ -90,35 +76,12 @@ public class AccueilController implements Initializable{
 				fin_sejour.setValue(debut_sejour.getValue());
 			}
 		});
-        Image image1 = new Image("https://www.horizon-provence.com/pont-avignon/photos/pont-saint-benezet08.jpg");
-        ImageView img1 = new ImageView();
-        img1.setImage(image1);
-        Image image2 = new Image("https://www.rustica.fr/images/247913j-l760-h550.jpg");
-        img2.setImage(image2);
-        Image image3 = new Image("https://www.josera.fr/media/magefan_blog/Pferd_Ausritt_shutterstock_1883230375_Ratgeber-Headerbild_1905x1040.jpg");
-        img3.setImage(image3);
-        Image image4 = new Image("https://www.cabanes-de-france.com/wp-content/uploads/2018/06/clarisse-creaphotos-7.jpg");
-        img4.setImage(image4);
-        Image image5 = new Image("https://resize2.prod.docfr.doc-media.fr/rcrop/480,280,center-middle/img/var/doctissimo/storage/images/fr/www/famille/maison/jardinage/661232-2-fre-FR/jardinage.jpg");
-        img5.setImage(image5);
-        
-        
-        // get all voyages
- 		listAllVoyages = this.getAllVoyages();
-
- 		// init datepickers
- 		LocalDate today = LocalDate.now();
- 		updateDatePicker(debut_sejour, today);
- 		updateDatePicker(fin_sejour, today);
-
- 		debut_sejour.valueProperty().addListener((ov, oldValue, newValue) -> {
- 			updateDatePicker(fin_sejour, debut_sejour.getValue());
- 			if (fin_sejour.getValue().isBefore(debut_sejour.getValue())){
- 				fin_sejour.setValue(debut_sejour.getValue());
- 			}
- 		});
 	}
-	
+
+
+
+        
+
 	@FXML
 	protected void onSearchButtonClick(ActionEvent event) throws IOException {
 		if (txtRecherche.getText().length() > 1) {
@@ -193,6 +156,17 @@ public class AccueilController implements Initializable{
 			}
 		} ).start();
 	}
+
+	public void updateDatePicker(DatePicker dp, LocalDate startLocalDate){
+		// restriction sur date picker
+		dp.setDayCellFactory(picker -> new DateCell() {
+			public void updateItem(LocalDate date, boolean empty) {
+				super.updateItem(date, empty);
+				setDisable(empty || date.compareTo(startLocalDate) < 0 );
+			}
+		});
+	}
+	
 	public void goToAccueil(ActionEvent event) throws IOException {
 		try {
 			System.out.println("Vous êtes sur la page d'accueil");
@@ -207,6 +181,4 @@ public class AccueilController implements Initializable{
 			System.err.println(e.getLocalizedMessage());
 		}
 	}
-
 }
-
